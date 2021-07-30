9b4938620708c480a56914358e611762966641b7
msdesai1499 <msdesai@mitaoe.ac.in>
2021-07-30 09:55:29 +0530

    Commit

---
+++ Striver STL/Code2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	array<int, 5> arr = {1, 5, 6};
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+	array<int, 5> a;
+	a.fill(10);
+	for (int i = 0; i < 5; i++)
+	{
+		cout << a[i] << endl;
+	}
+
+	// iterators
+}
